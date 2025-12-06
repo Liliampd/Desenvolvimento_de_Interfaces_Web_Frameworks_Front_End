b80dba8a4652d7fa8d43fd9e6882d9cad8221831
Liliam Santos <liliam.santos@pditabira.com>
2025-12-05 21:07:03 -0300

    commit 1
    Primeiro commit deste projeto.

---
+++ assets/js/scripts.js
@@ -0,0 +1,9 @@
+// Código popover
+
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+// Código Tooltips
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
