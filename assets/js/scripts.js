63f25d019f02710f96e112e1c6938c237d760920
Liliam Santos <liliam.santos@pditabira.com>
2025-12-13 00:54:11 -0300

    commit site pronto com modal

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,3 +7,12 @@ const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstra
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+//MODAL
+
+const exampleModalEl = document.getElementById('modalPortifolio');
+
+exampleModalEl.addEventListener('show.bs.modal', function(event)
+{
+    document.getElementById('modalImage').src = event.relatedTarget.getAttribute('data-img');
+});
